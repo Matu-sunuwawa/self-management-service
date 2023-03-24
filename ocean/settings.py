23b0cce2573b42a5fd5400ca680bc9b6c1182d89
Matyas Sina <matyassinaadugna@gmail.com>
2023-03-24 14:53:11 -0700

    initial commit

--- ocean/settings.py
+++ ocean/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ocean.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'DCKZvk5ax4lqzI2y4816',
+        'HOST': 'containers-us-west-82.railway.app',
+        'PORT': '7187',
     }
 }
 
